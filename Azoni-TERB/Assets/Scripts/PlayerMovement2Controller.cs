53739955ec44c76594735e163e38d29b32c8160f
FerGLDev <ferchojunior1@hotmail.com>
2022-05-07 18:46:34 -0500

    otros test

--- Azoni-TERB/Assets/Scripts/PlayerMovement2Controller.cs
+++ Azoni-TERB/Assets/Scripts/PlayerMovement2Controller.cs
@@ -2,12 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class PlayerMovement2Controller : MonoBehaviour
 {
     [SerializeField] JoystickLeft jl;
     private float speed = 5f;
     public bool rightAreaRotateCamera = false;
     private Rigidbody rb;
+    private float turnSmoothtime;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -19,8 +22,8 @@ public class PlayerMovement2Controller : MonoBehaviour
 		rb.MovePosition(transform.position + moveX * Time.fixedDeltaTime + moveY  * Time.fixedDeltaTime);
         //Transform stick = jl.stick.transform;
         //Vector3 pos = stick.transform.position;
-
-        //transform.rotation = Quaternion.Euler(0f,Mathf.Atan(pos.y/pos.x)*100f,0f);
+   
+        //transform.rotation = Quaternion.Euler(0f,Mathf.SmoothDampAngle(transform.eulerAngles.y,Mathf.Atan(JoystickLeft.positionY/JoystickLeft.positionX)*Mathf.Rad2Deg, ref turnSmoothtime,0.5f),0f);
         
 	}
     // Update is called once per frame
