6e3276c5e5890ffe72fcb4a2bbb8e92adab44990
javierpenaf <ejpf2004@gmail.com>
2023-03-16 14:16:55 -0500

    .

--- Azoni-TERB/Assets/Scripts/LevelLocker.cs
+++ Azoni-TERB/Assets/Scripts/LevelLocker.cs
@@ -1,14 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LevelLocker : MonoBehaviour
 {
     [SerializeField] GameObject sceneManager;
     SceneController sceneController;
     public GameObject message;
+    public Image star1;
+    public Image star2;
+    public Image star3;
+    public float maxScore;
+    public float score;
     public Sprite lockedSprite;
     public Sprite unlockedSprite;
+    public Sprite activeStar;
+    public Sprite inactiveStar;
     public float tiempo;
     public string Scene;
     public bool isLevelUnlocked;
@@ -18,9 +26,12 @@ public class LevelLocker : MonoBehaviour
     {
         sceneController= sceneController = sceneManager.GetComponent<SceneController>();
         message.SetActive(false);
+        isLevelUnlocked = false;
     }
     void Update()
     {
+        Score(maxScore, score);
+
         if (isLevelUnlocked)
         {
             gameObject.GetComponent<SpriteRenderer>().sprite = unlockedSprite;
@@ -32,8 +43,34 @@ public class LevelLocker : MonoBehaviour
         }
     }
 
-
-
+    void Score(float maxScore,float score)
+    {
+        double thres1 = maxScore * 0.5;
+        double thres2 = maxScore * 0.75;
+        if (maxScore == score)
+        {
+            star1.sprite = activeStar;
+            star2.sprite = activeStar;
+            star3.sprite = activeStar;
+            isLevelUnlocked = true;
+        }else if (score >= thres2)
+        {
+            star1.sprite = activeStar;
+            star2.sprite = activeStar;
+            star3.sprite = inactiveStar;
+        }else if (score >= thres1)
+        {
+            star1.sprite = activeStar;
+            star2.sprite = inactiveStar;
+            star3.sprite = inactiveStar;
+        }
+        else if (score < thres1)
+        {
+            star1.sprite = inactiveStar;
+            star2.sprite = inactiveStar;
+            star3.sprite = inactiveStar;
+        }
+    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
